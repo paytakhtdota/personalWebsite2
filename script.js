1fa658b88b9d410dd81f794a04ca3e979fd4a091
Mo Ansari <paytakhtdota@gmail.com>
2023-09-22 21:28:34 -0700

    nav bar added

---
+++ script.js
@@ -0,0 +1,7 @@
+function mousehover() {
+document.getElementById("logo-img").src = "./img/logo2.jpg";
+}
+
+function mouseleave() {
+    document.getElementById("logo-img").src = "./img/logo1.jpg";
+    }
