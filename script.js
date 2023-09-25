6c19eb21949faf4bf943deb7b53741141395e00a
Mo Ansari <paytakhtdota@gmail.com>
2023-09-24 22:56:45 -0700

    navbar is responsive now

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ document.getElementById("logo-img").src = "./img/logo2.jpg";
 function mouseleave() {
     document.getElementById("logo-img").src = "./img/logo1.jpg";
     }
+
+    function closeMenu() {
+        document.getElementById("mini-menu").style.display="none";
+        }
+
+        function openMenu() {
+            document.getElementById("mini-menu").style.display="flex";
+            }
