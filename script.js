00d9862fd60338e3997f1abbaf55246d1ace9fd2
Mo Ansari <paytakhtdota@gmail.com>
2023-09-25 21:35:30 -0700

    fixed navbar added - fixed navbar is respansible now

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
+const desiredHeightPercentage = 96;
+const viewportHeight = window.innerHeight;
+const desiredHeightInPixels = (desiredHeightPercentage / 100) * viewportHeight;
+
 function mousehover() {
-document.getElementById("logo-img").src = "./img/logo2.jpg";
+    document.getElementById("logo-img").src = "./img/logo2.jpg";
 }
 
 function mouseleave() {
     document.getElementById("logo-img").src = "./img/logo1.jpg";
+}
+
+function closeMenu() {
+    document.getElementById("mini-menu").style.display = "none";
+}
+
+function openMenu() {
+    document.getElementById("mini-menu").style.display = "flex";
+}
+
+function fixedNavBar(){
+    const fixnav = document.getElementById("fixed-navbar");
+    if(window.scrollY >= desiredHeightInPixels){
+        fixnav.style.display = "block";
+    }
+    else{
+        fixnav.style.display = "none";
     }
 
-    function closeMenu() {
-        document.getElementById("mini-menu").style.display="none";
-        }
+}
 
-        function openMenu() {
-            document.getElementById("mini-menu").style.display="flex";
-            }
+window.addEventListener('scroll', fixedNavBar);
